8bf95f41660b1c4619271c2c551d2d85f99c9e6c
Savindu@Pasintha <64083148+savindu-pasintha@users.noreply.github.com>
2023-03-22 16:03:43 +0530

    Update StyleComponent and Jquery Using Custom Styles.js

--- StyleComponent and Jquery Using Custom Styles.js
+++ StyleComponent and Jquery Using Custom Styles.js
@@ -2,13 +2,14 @@ import React, { useEffect } from 'react'
 import MUIDataTable from "mui-datatables";
 
 //styles compoennts
-// import styled from 'styled-components';
-import styled,{keyframes} from '@emotion/styled'
+import styled,{keyframes} from 'styled-components';
+// import styled,{keyframes} from '@emotion/styled'
 
 
 
 //jquery
 import $ from "jquery";
+import { style } from '@mui/system';
 
 export const MUIDataTableStyles =  styled(MUIDataTable)`background-color:red;
 &:hover {
@@ -16,11 +17,36 @@ export const MUIDataTableStyles =  styled(MUIDataTable)`background-color:red;
   }`;
 export const Buttonx = styled.button`background-color:white;border:none;width:150px;height:50px;`;
 
+export const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+const Spinner = styled.div`
+	animation: ${rotate} 1s linear infinite;
+	position: relative;
+	margin: auto;
+	width: 1rem;
+	height: 1rem;
+	border-radius: 50%;
+	border: 6px solid white;
+	border-right-color: transparent;
+    background-color:black;
+    padding:1rem;
+    color:white;
+`;
+const CustomSpiner = styled.div`
+width:150px;padding:10px;
+background-color:black;
+`;
 
 const CustomCss = () => {
     useEffect(()=>{
         $(".MuiTableCell-root").css("background-color", "brown")
-         $("tr").css("border", "3px solid white")
+        $("tr").css("border", "3px solid white")
         $("th").append("<Buttonx>ssss</Buttonx>")
         $('.tss-hj53wm-MUIDataTableToolbar-titleText').remove()
         // $(".tss-hj53wm-MUIDataTableToolbar-titleText").css("display", "none")
@@ -33,6 +59,7 @@ const CustomCss = () => {
     },[])
   return (
     <div>
+        <CustomSpiner><Spinner /></CustomSpiner>
       <div>
         <MUIDataTableStyles
           title={"Employee List"}
